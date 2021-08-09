9f496209a31f4db911e1ca9641ebd59723437172
Choi.SY <sollly0829@gmail.com>
2021-08-09 11:16:42 +0900

    Ex04 public/private 변수 지정 차이

--- src/day10_ClassVariable/Ex04_MainClass.java
+++ src/day10_ClassVariable/Ex04_MainClass.java
@@ -5,10 +5,11 @@ public class Ex04_MainClass {	//인스턴스변수
 	public static void main(String[] args) {
 		
 		Ex04_Variable ex = new Ex04_Variable();
+		ex.cnt = 12345;		//cnt변수가 public이기 때문에 사용 가능(private시 에러)
 		ex.func01();
 		ex.func02();
 		
-		Ex04_Variable ex1 = new Ex04_Variable();
-		ex1.func01();
+		//Ex04_Variable ex1 = new Ex04_Variable();
+		//ex1.func01();
 	}
 }
