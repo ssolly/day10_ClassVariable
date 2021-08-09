39a676328973a43b268f5b5ea00f5c926b582cc8
Choi.SY <sollly0829@gmail.com>
2021-08-09 11:04:53 +0900

    Ex04 인스턴스 변수

---
+++ src/day10_ClassVariable/Ex04_MainClass.java
@@ -0,0 +1,14 @@
+package day10_ClassVariable;
+
+public class Ex04_MainClass {	//인스턴스변수
+
+	public static void main(String[] args) {
+		
+		Ex04_Variable ex = new Ex04_Variable();
+		ex.func01();
+		ex.func02();
+		
+		Ex04_Variable ex1 = new Ex04_Variable();
+		ex1.func01();
+	}
+}
---
+++ src/day10_ClassVariable/Ex04_Variable.java
@@ -0,0 +1,16 @@
+package day10_ClassVariable;
+
+public class Ex04_Variable {	//인스턴스
+
+	public int cnt = 10;
+	
+	public void func01() {
+		cnt++;
+		System.out.println("01 cnt : " + cnt);
+	}
+	
+	public void func02() {
+		cnt++;
+		System.out.println("02 cnt : " + cnt);
+	}
+}
